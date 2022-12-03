46e787ccd455d9f810cfaa99cf561845c20dcd92
MaksimZ91 <ak4202@mail.ru>
2022-12-03 16:47:03 +0800

    task 3 complited

---
+++ pythonLesson4_2task.py
@@ -0,0 +1,26 @@
+#Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
+N = int(input("Введите значение числа N: "))
+
+def factor(num):
+  count = 2
+  ls = []
+  while count <= num:  
+    if num % count == 0:
+        ls.append(count)
+        num = num / count
+        count = 2 
+    else: count +=1
+  return ls     
+
+print(factor(N))  
+
+
+
+
+
+
+
+
+
+   
---
+++ pythonLesson4_3task.py
@@ -0,0 +1,20 @@
+# Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+# Ввод: [1, 1, 2, 3, 4, 4, 4]
+# Вывод: [2, 3]
+
+ls = [1, 1, 2, 3, 4, 4, 4, 7, 7, 9, 2, 5]
+
+def task3(ls):
+  result = []
+  _setA = set(ls)
+  ls = sorted(ls) 
+  for i in range(len(ls)-1):
+    result.append(ls[i]) if ls[i] == ls[i+1] else ""
+  _setB = set(result) 
+  return list( _setA - _setB)
+ 
+print(task3(ls))
+  
+   
+    
+
